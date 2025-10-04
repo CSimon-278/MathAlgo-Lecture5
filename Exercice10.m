31c2ef64d88675a486e913049e45ce0d38170071
CSimon-278 <chauveausimon11@gmail.com>
2025-10-04 09:35:38 +0300

    Update Exercice10.m

--- Exercice10.m
+++ Exercice10.m
@@ -15,22 +15,22 @@ b2n = b2/sum(b2);
 [h2,w2] = freqz(b2,a2,1024);
 
 figure;
-subplot(2,1,1);
-zplane(h1,w1);
+subplot(2,2,1);
+plot(w1/pi,abs(h1));
 title("5-point Moving Average");
 
-subplot(2,1,2);
-zplane(h2,w2);
+subplot(2,2,3);
+plot(w2/pi,abs(h2));
 title("9-point Moving Average");
 
 [z1,p1,k1] = tf2zpk(b1n, a1);
 [z2,p2,k2] = tf2zpk(b2n, a2);
 
 figure;
-subplot(1,2,1);
+subplot(2,2,2);
 zplane(z1,p1);
 title("Zeros/Poles: 5-point MA");
 
-subplot(1,2,2);
+subplot(2,2,4);
 zplane(z2,p2);
 title("Zeros/Poles: 9-point MA");
